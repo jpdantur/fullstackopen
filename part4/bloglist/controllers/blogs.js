032da8149b414f331c59ae2029fb72b46c3bfc35
jpdantur <jpdantur@gmail.com>
2020-08-02 23:49:01 -0300

    4.8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,17 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   const blog = new Blog(body);
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const helper = require("./test_helper");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+test("a specific blog is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  const titles = response.body.map((r) => r.title);
+
+  expect(titles).toContain("React patterns");
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "async/await simplifies making async calls",
+    author: "Pepe Argento",
+    url: "http://pepeargento.com",
+    likes: 3,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((r) => r.title);
+  expect(titles).toContain("async/await simplifies making async calls");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
