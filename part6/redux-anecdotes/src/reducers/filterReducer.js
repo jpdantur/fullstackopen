2f4af7a35937d79bb89a63047a613d0c3d1cf139
Juan Dantur <jpdantur@gmail.com>
2020-09-21 21:44:19 -0300

    6.12

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+const initialState = ''
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_FILTER':
+            return action.text
+        default:
+            return state
+    }
+}
+
+export const setFilter = text => {
+    return {
+        type: 'SET_FILTER',
+        text
+    }
+}
+
+export default reducer
---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,27 @@
+const initialState = ''
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_NOTIFICATION':
+            return action.message
+        case 'REMOVE_NOTIFICATION':
+            return ''
+        default:
+            return state
+    }
+}
+
+export const setNotification = message => {
+    return {
+        type: 'SET_NOTIFICATION',
+        message
+    }
+}
+
+export const removeNotification = () => {
+    return {
+        type: 'REMOVE_NOTIFICATION'
+    }
+}
+
+export default reducer
